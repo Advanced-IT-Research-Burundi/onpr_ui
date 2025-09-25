0cfee65fd1b087f3fbdb718ff37124ab1fd5a7da
schammahmuhizi <38748323+schammahmuhizi@users.noreply.github.com>
2025-09-25 11:10:37 +0200

    Adding footer

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
-
+import Footer from "./Footer"
 function Layout() {
   return (
     <div>
       <h1>My App</h1>
       <Outlet />
+      <Footer />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import './assets/styles/footer.css';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
