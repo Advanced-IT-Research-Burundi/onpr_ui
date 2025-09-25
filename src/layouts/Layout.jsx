a12bff1597fec5022fc0178af3075a8230b7f509
irumvabric <irumvabric#@gmail.com>
2025-09-25 12:01:41 +0200

    Changed Color Footer + padding logo header

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
+import Footer from "./Footer"
+import Header from "./Header"
+import TopHeader from "./TopHeader"
 
 function Layout() {
   return (
     <div>
-      <SubHeader/>
+      <TopHeader/>
       <Header/>
       <h1>My App</h1>
       <Outlet />
--- src/layouts/TopHeader.jsx
+++ src/layouts/TopHeader.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-export default function TopBar() {
+export default function TopHeader() {
   return (
     <div className="bg-light py-2">
       <div className="container d-flex justify-content-between align-items-center">
         {/* Left side: Phone and Email */}
         <div className="d-flex align-items-center">
           <div className="d-flex align-items-center me-4">
-            <i className="pi pi-phone"></i>
+            <i className="pi pi-phone text-success px-2"></i>
             <span>+257 22 27 62 29</span>
           </div>
 
           <div className="d-flex align-items-center">
-            <i className="pi pi-envelope"></i>
+            <i className="pi pi-envelope text-success px-2"></i>
             <span>onpr_burundi@onpr.bi</span>
           </div>
         </div>
 
         {/* Right side: Button */}
-        <button className="btn btn-danger fw-bold">Se Connecter</button>
+        <button className="btn btn-danger fw-medium">Se Connecter</button>
       </div>
     </div>
   );
