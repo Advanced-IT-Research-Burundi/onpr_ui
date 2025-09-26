bcb6438bf47ee7be100f6ea29f1d9dd55b2f56f7
schammahmuhizi <38748323+schammahmuhizi@users.noreply.github.com>
2025-09-26 12:04:01 +0200

    Load jquery

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import AboutScreen from "./pages/AboutScreen.jsx";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<HomeLayout />}>
         <Route path="/home" element={<HomeLayout />}>
           <Route path="./about" element={<AboutScreen />} />
         </Route>
